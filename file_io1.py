6a0521dcef0fd7e87577fd6decb665284c77e8d5
vivek sah <viveksha129@gmail.com>
2025-10-03 20:23:04 +0530

    file_handling

---
+++ file_io1.py
@@ -0,0 +1,24 @@
+
+# with open("Demo.txt", "r") as myfile:
+#     data = myfile.read()
+#     print(data)
+
+
+# with open(r"C:\Users\user\Desktop\SQL PROGRESS.txt", "r") as f:
+#     data = f.read()
+#     print(data)
+#     print(type(data))
+
+
+with open(r"C:\Users\user\Documents\excel files\SQL - Retail Sales Analysis_utf .csv","r") as p:
+    pata=p.read()
+    print(pata)
+    p.close()
+
+
+# with open(r"D:\python files\vivek_sah\file_I\O\demo.txt","r") as d:
+#     daka=d.read()
+#     print(daka)
+
+
+
