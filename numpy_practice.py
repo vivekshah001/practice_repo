985e5f335d05a302a959a04a3ee094d673a99302
vivek sah <viveksha129@gmail.com>
2025-10-09 23:07:44 +0530

    numpy

--- numpy_practice.py
+++ numpy_practice.py
@@ -12,15 +12,17 @@ import numpy as np
 
 # print(np.identity(3))
 
-a=np.array([[1,2,3],[4,5,6]])
+a=np.array([[1,2,3],[4,5,6],[7,8,9]])
 
 # print(a.shape)
 # shape=a.reshape(2,3)
 # print(shape)
 
-# print(a)
+print(a)
 
 # print(a.size)
 # print(a.itemsize)
 # print(a.astype(np.int32))
-print(a.dtype)
+# print(a.dtype)
+
+print(a[::2,1::2])
