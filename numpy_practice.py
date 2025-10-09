4327874e72f390238ef448374756ec3c5e57486d
vivek sah <viveksha129@gmail.com>
2025-10-09 11:24:29 +0530

    numpy_practice

---
+++ numpy_practice.py
@@ -0,0 +1,26 @@
+
+
+import numpy as np
+
+# print(np.random.randn(2,3))
+
+# print(np.random.randint(2,10))
+
+# print(np.random.choice([1,2,3,4]))
+
+# print(np.linspace(-10,10,50))
+
+# print(np.identity(3))
+
+a=np.array([[1,2,3],[4,5,6]])
+
+# print(a.shape)
+# shape=a.reshape(2,3)
+# print(shape)
+
+# print(a)
+
+# print(a.size)
+# print(a.itemsize)
+# print(a.astype(np.int32))
+print(a.dtype)
